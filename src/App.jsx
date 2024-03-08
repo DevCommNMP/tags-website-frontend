77f2818d7f81b99cb8877b03e8e01c0537751fd1
Aaditya Singh <singhaditya2763@gmail.com>
2024-03-08 12:46:17 +0530

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { Provider } from 'react-redux'
 import './App.css'
 import Store from './redux/store/store'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Homepage from './pages/Homepage.jsx'
+import Homepage from './pages/Homepage.jsx'
+import Login from './components/Auth/login/Login.jsx'
 import SignUp from './components/Auth/signup/SignUp.jsx'
 
 function App() {
   return (
     <>
       <Provider store={Store}>
-        {/* <Homepage /> */}
-        <SignUp />
+      <BrowserRouter>
+        <div className="d-flex flex-column" style={{ overflow: "hidden" }}>
+
+          <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/login" element={<Login />} />
+           <Route exact path="/signup" element={<SignUp/>} />
+           
+          </Routes>
+        </div>
+      </BrowserRouter>
       </Provider>
      </>
   )
