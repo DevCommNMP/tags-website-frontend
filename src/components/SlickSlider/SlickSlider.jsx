e8c1aef01b706831eb60293922bf446971acfa9f
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-03-09 15:41:37 +0530

    changed theme color and banners

--- src/components/SlickSlider/SlickSlider.jsx
+++ src/components/SlickSlider/SlickSlider.jsx
@@ -14,29 +14,29 @@ export default function SlickSlider() {
   return (
     <Slider {...settings}>
       <li className="single-hero-slider single-animation-wrap sliderImg1">
-        <h1>Slide1</h1>
+        <h1> </h1>
       </li>
 
       <li className="single-hero-slider single-animation-wrap sliderImg2">
-        <h1>Slide2</h1>
+        <h1> </h1>
       </li>
 
       <li className="single-hero-slider single-animation-wrap sliderImg1">
-        <h1>Slide3</h1>
+        <h1> </h1>
       </li>
 
       <li className="single-hero-slider single-animation-wrap sliderImg2">
-        <h1>Slide4</h1>
+        <h1> </h1>
       </li>
 
       <li className="single-hero-slider single-animation-wrap sliderImg1">
-        <h1>Slide5</h1>
+        <h1> </h1>
       </li>
       <li className="single-hero-slider single-animation-wrap sliderImg2">
-        <h1>Slide6</h1>
+        <h1> </h1>
       </li>
       <li className="single-hero-slider single-animation-wrap sliderImg1">
-        <h1>Slide7</h1>
+        <h1> </h1>
       </li>
     </Slider>
   )
