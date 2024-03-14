3aee11924392b298b42b3472ae08a556a58aec57
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-03-14 17:10:53 +0530

    update

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import Checkout from '../components/Checkout'
+
+const CheckoutPage = () => {
+  return (
+    <>
+    <Header />
+    <Checkout />
+    <Footer />
+    </>
+  )
+}
+
+export default CheckoutPage
