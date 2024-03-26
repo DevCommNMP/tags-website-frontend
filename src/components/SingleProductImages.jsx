2711bbe788924cfbc02a9baea420de02faa484e0
Aaditya Singh <singhaditya2763@gmail.com>
2024-03-26 21:55:06 +0530

    updated

--- src/components/SingleProductImages.jsx
+++ src/components/SingleProductImages.jsx
@@ -1,25 +1,13 @@
-import { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Slider from 'react-slick';
 
-// Import product images here
-import product161 from '../assets/imgs/shop/product-16-1.jpg';
-import product162 from '../assets/imgs/shop/product-16-2.jpg';
-import product163 from '../assets/imgs/shop/product-16-3.jpg';
-import product164 from '../assets/imgs/shop/product-16-4.jpg';
-import product165 from '../assets/imgs/shop/product-16-5.jpg';
-import product166 from '../assets/imgs/shop/product-16-6.jpg';
-import product167 from '../assets/imgs/shop/product-16-7.jpg';
-
-const SingleProductImages = ({ product,data }) => {
-  // Destructure product properties if necessary
-
+const SingleProductImages = ({ product }) => {
   const { productImage, productSubImages } = product;
-  // console.log(product)
-  // Ensure productImage and productSubImages are arrays
-  const productImagesArr = Array.isArray(productImage) ? productSubImages : [];
-  const productSubImagesArr = Array.isArray(productSubImages) ? productSubImages : [];
-  // console.log(productSubImagesArr)
 
+  // Ensure productImage and productSubImages are arrays
+  const productImagesArr = Array.isArray(productImage) ? [] : productSubImages;
+  const productSubImagesArr = Array.isArray(productSubImages) ? productSubImages : [];
+console.log(productImagesArr,productSubImagesArr)
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const sliderRef = useRef();
 
@@ -47,11 +35,9 @@ const SingleProductImages = ({ product,data }) => {
   return (
     <div className="col-md-6 col-sm-12 col-xs-12 mb-md-0 mb-sm-5">
       <div className="detail-gallery">
-        <span className="zoom-icon">
-          <i className="fi-rs-search"></i>
-        </span>
         <div className="product-image-slider">
           <Slider ref={sliderRef} {...settings}>
+            {/* Render main product image */}
             {productImagesArr.map((image, index) => (
               <div key={index}>
                 <img src={image} alt={`product image ${index}`} />
@@ -61,6 +47,7 @@ const SingleProductImages = ({ product,data }) => {
         </div>
         <div className="slider-nav-thumbnails">
           <Slider {...settingsThumb}>
+            {/* Render product sub-images */}
             {productSubImagesArr.map((image, index) => (
               <div
                 key={index}
