f0bf3c07f91679f37bdf2ea2bdb0f71607db663b
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-06-11 17:34:08 +0530

    fixed slider

--- src/components/SingleProductImages.jsx
+++ src/components/SingleProductImages.jsx
@@ -1,74 +1,68 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Slider from "react-slick";
 import ImageZoom from "./ImageZoom";
 
-const SingleProductImages = ({ product }) => {
-
+function SingleProductImages({ product }) {
   const [nav1, setNav1] = useState(null);
   const [nav2, setNav2] = useState(null);
-  let sliderRef1 = useRef(null);
-  let sliderRef2 = useRef(null);
+  const sliderRef1 = useRef(null);
+  const sliderRef2 = useRef(null);
 
   useEffect(() => {
     setNav1(sliderRef1.current);
     setNav2(sliderRef2.current);
   }, []);
 
-  const { productImage, productSubImages } = product;
-// console.log(productSubImages);
-// console.log(productImage)
-  // Ensure productImage and productSubImages are arrays
+  const { productSubImages } = product;
   const productImagesArr = Array.isArray(productSubImages) ? productSubImages : [];
-  const productSubImagesArr = Array.isArray(productSubImages) ? productSubImages : [];
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", color:"white",backgroundColor:"grey",borderRadius:"100%"}}
-        onClick={onClick}
-      />
-    );
-  }
-  
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block" , color:"white",backgroundColor:"grey",borderRadius:"100%"}}
-        onClick={onClick}
-      />
-    );
-  }
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 800,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // nextArrow: <SampleNextArrow />,
-    // prevArrow: <SamplePrevArrow />
-  };
+
+  const Arrow = ({ className, style, onClick }) => (
+    <div
+      className={className}
+      style={{
+        ...style,
+        color: "black",
+        backgroundColor: "black",
+        border: "1px solid white",
+        borderRadius: "100%",
+        // width: "30px",
+        // height: "30px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+
+      }}
+      onClick={onClick}
+    />
+  );
 
   const settingsThumb = {
     dots: false,
-    infinite: false,
+    infinite: true,
     speed: 800,
     slidesToShow: 4,
     slidesToScroll: 1,
     vertical: true,
     verticalSwiping: true,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
+    nextArrow: <Arrow direction="next" />,
+    prevArrow: <Arrow direction="prev" />,
+  };
 
+  const settingsMain = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
     <div className="col-md-6 col-sm-12 col-xs-12 mb-md-0 mb-sm-5">
-      <div className="detail-gallery row" style={{}}>
+      <div className="detail-gallery row">
+        {/* Thumbnails */}
         <div className="slider-nav-thumbnails col-2">
-          <Slider {...settingsThumb} asNavFor={nav1} ref={sliderRef2}>
+          <Slider {...settingsThumb} asNavFor={nav1} ref={sliderRef2} swipeToSlide focusOnSelect>
             {productImagesArr.map((image, index) => (
               <div key={index}>
                 <img src={image} alt={`product image ${index}`} />
@@ -76,8 +70,9 @@ const SingleProductImages = ({ product }) => {
             ))}
           </Slider>
         </div>
+        {/* Main Image */}
         <div className="product-image-slider col-10">
-          <Slider {...settings} asNavFor={nav2} ref={sliderRef1}>
+          <Slider {...settingsMain} asNavFor={nav2} ref={sliderRef1}>
             {productImagesArr.map((image, index) => (
               <div key={index} className="image-container">
                 <ImageZoom imageUrl={image} />
@@ -88,6 +83,6 @@ const SingleProductImages = ({ product }) => {
       </div>
     </div>
   );
-};
+}
 
 export default SingleProductImages;
