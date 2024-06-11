3b0cd974f72be839c5820eed5a626dea7d3f9dac
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-06-11 14:42:10 +0530

    formated the code

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -10,7 +10,7 @@ import SingleProductImages from "../components/SingleProductImages";
 import starRating from "../assets/imgs/theme/rating-stars.png";
 import ProductInfo from "../components/ProductInfo";
 import { discount as globalDiscount } from "../utils/baseUrl";
-import shoeSizeChart from "../assets/imgs/Shoe-sizeimage/sizeChart.jpg"
+import shoeSizeChart from "../assets/imgs/Shoe-sizeimage/sizeChart.jpg";
 const Product = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -27,21 +27,17 @@ const Product = () => {
   const [error, setError] = useState("");
   const [selectedIndex, setSelectedIndex] = useState(0);
 
-
-
   const handleproductImageChange = async (color) => {
-    console.log(color)
+    console.log(color);
     const lowercaseColor = color.toLowerCase();
-    const productIndexWithSelectedColor = particularproduct.findIndex(product => 
-        product.colorsAvailable && product.colorsAvailable.length && 
-        product.productName.toLowerCase().includes(lowercaseColor)
+    const productIndexWithSelectedColor = particularproduct.findIndex(
+      (product) => product.colorsAvailable && product.colorsAvailable.length && product.productName.toLowerCase().includes(lowercaseColor)
     );
-    console.log(productIndexWithSelectedColor)
+    console.log(productIndexWithSelectedColor);
     if (productIndexWithSelectedColor !== -1) {
-        // console.log(productIndexWithSelectedColor);
-        setSelectedIndex(productIndexWithSelectedColor);
+      // console.log(productIndexWithSelectedColor);
+      setSelectedIndex(productIndexWithSelectedColor);
     }
-  
   };
 
   const handleBuyNow = async (particularproduct, selectedColor, selectedSize, quantity) => {
@@ -58,7 +54,7 @@ const Product = () => {
 
   const product = particularproduct?.[selectedIndex];
   const productTitle = product?.title;
-  const productName=product?.productName;
+  const productName = product?.productName;
   const productRating = product?.rating;
   const productDescription = product?.description;
   const productSellingPrice = product?.SellingPrice;
@@ -69,7 +65,6 @@ const Product = () => {
   const productPrice = productDiscount
     ? productSellingPrice * (1 - productDiscount / 100)
     : productSellingPrice * (1 - globalDiscount / 100);
-
 
   const cartHandler = async (item, selectedColor, selectedSize, quantity) => {
     if (selectedColor && selectedSize) {
@@ -93,10 +88,9 @@ const Product = () => {
     }
   };
 
-  const handleColorSelection =async (color) => {
-  
-   setSelectedColor(color)
-  
+  const handleColorSelection = async (color) => {
+    setSelectedColor(color);
+
     handleproductImageChange(color);
   };
 
@@ -112,12 +106,11 @@ const Product = () => {
   };
   useEffect(() => {
     dispatch(fetchParticularProduct(id));
-  }, [ dispatch]);
-  
+  }, [dispatch]);
 
   return particularproduct ? (
     <>
-      <ToastContainer 
+      <ToastContainer
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
@@ -147,7 +140,7 @@ const Product = () => {
               <div className="product-detail accordion-detail">
                 <div className="row mb-50 mt-30">
                   {product && <SingleProductImages product={product} />}
-                  
+
                   <div className="col-md-6 col-sm-12 col-xs-12">
                     <div className="detail-info pr-30 pl-30">
                       <h2 className="title-detail">{productTitle}</h2>
@@ -162,20 +155,30 @@ const Product = () => {
                           <span className="font-small ml-5 text-muted">(32 reviews)</span>
                         </div>
                       </div>
-                      <div className="clearfix product-price-cover" >
+                      <div className="clearfix product-price-cover">
                         <div className="product-price primary-color float-left" style={{ fontSize: "2rem", color: "red", fontWeight: 800 }}>
                           <span>
-                            <span className="save-price font-md color ml-15" style={{color:"red",fontSize:"25px",marginRight:20}} >-{productDiscount ? productDiscount : globalDiscount}% <span style={{color:"black"}}>Off </span></span>
-                          <div style={{display:"flex",alignItems:"center"}}> <span style={{fontSize:5
-                            ,color:"grey",marginLeft:10,}}>M.R.P. :</span> <span className="old-price font-md ml-5" style={{fontSize:15, }}>&#8377;{productSellingPrice}</span></div>
-
+                            <span className="save-price font-md color ml-15" style={{ color: "red", fontSize: "25px", marginRight: 20 }}>
+                              -{productDiscount ? productDiscount : globalDiscount}% <span style={{ color: "black" }}>Off </span>
+                            </span>
+                            <div style={{ display: "flex", alignItems: "center" }}>
+                              {" "}
+                              <span style={{ fontSize: 5, color: "grey", marginLeft: 10 }}>M.R.P. :</span>{" "}
+                              <span className="old-price font-md ml-5" style={{ fontSize: 15 }}>
+                                &#8377;{productSellingPrice}
+                              </span>
+                            </div>
                           </span>
-                          <span >&#8377; {productPrice}</span>
-                          <span className="save-price font-md color ml-15" style={{color:"green",fontSize:"16px"}}>Inc. all taxes</span>
+                          <span>&#8377; {productPrice}</span>
+                          <span className="save-price font-md color ml-15" style={{ color: "green", fontSize: "16px" }}>
+                            Inc. all taxes
+                          </span>
                         </div>
                       </div>
                       <div className="short-desc mb-30">
-                        <p className="font-lg" style={{color:"black",fontSize:"25px"}}>{productDescription}</p>
+                        <p className="font-lg" style={{ color: "black", fontSize: "25px" }}>
+                          {productDescription}
+                        </p>
                       </div>
                       <div className="attr-detail attr-size mb-3">
                         <strong className="mr-10">Size</strong>
@@ -184,7 +187,10 @@ const Product = () => {
                             product.sizesAvailable &&
                             product.sizesAvailable.map((item, index) => (
                               <li key={index}>
-                                <a onClick={() => handleSizeSelection(item.size, item.quantity)} className={selectedSize === item.size ? "selected" : "notselected"}>
+                                <a
+                                  onClick={() => handleSizeSelection(item.size, item.quantity)}
+                                  className={selectedSize === item.size ? "selected" : "notselected"}
+                                >
                                   {item.size}
                                 </a>
                               </li>
@@ -197,10 +203,12 @@ const Product = () => {
                           {product &&
                             product.colorsAvailable &&
                             product.colorsAvailable.map((item, index) => (
-                              <li key={index} >
+                              <li key={index}>
                                 <a
-                                  onClick={() => handleColorSelection(item)} 
-                                  className={`product-color-box product-${item.replace(/[\s./]+/g, "-").toLowerCase()} ${selectedColor === item ? "selected" : "notselected"}` }
+                                  onClick={() => handleColorSelection(item)}
+                                  className={`product-color-box product-${item.replace(/[\s./]+/g, "-").toLowerCase()} ${
+                                    selectedColor === item ? "selected" : "notselected"
+                                  }`}
                                 ></a>
                               </li>
                             ))}
@@ -224,7 +232,7 @@ const Product = () => {
                           <button
                             type="button"
                             className="border bg-white  text-brand radius button button-add-to-cart"
-                            onClick={() => cartHandler(product,selectedColor,selectedSize,quantity)}
+                            onClick={() => cartHandler(product, selectedColor, selectedSize, quantity)}
                             disabled={soldOut}
                           >
                             <i className="fi-rs-shopping-cart"></i>Add to cart
@@ -232,7 +240,7 @@ const Product = () => {
                           <button
                             type="button"
                             className="button button-primary button-add-to-cart ml-5"
-                            onClick={()=>handleBuyNow(product,selectedColor,selectedSize,quantity)}
+                            onClick={() => handleBuyNow(product, selectedColor, selectedSize, quantity)}
                             disabled={soldOut}
                           >
                             <i className="fi-rs-shopping-cart"></i>Buy Now
@@ -241,9 +249,7 @@ const Product = () => {
                         {error && <p className="text-danger">{error}</p>}
                       </div>
                       <div className="font-xs">
-                        <ul className="mr-50 float-start">
-                          
-                        </ul>
+                        <ul className="mr-50 float-start"></ul>
                         <ul className="float-start">
                           <li className="mb-5">
                             Product-Id:<span className="in-stock text-brand ml-5">{product?.productName}</span>
@@ -255,7 +261,7 @@ const Product = () => {
                           <li className="mb-5">
                             Brand: <span className="text-brand">{product?.brand}</span>
                           </li>
-                        
+
                           <li>
                             Stock:
                             <span className="in-stock text-brand ml-5">{stock} Items In Stock</span>
@@ -266,32 +272,32 @@ const Product = () => {
                   </div>
                 </div>
                 <table>
-      <thead>
-      <tr style={{fontSize:"25px",marginTop:"50px",color:"black"}}> Size Chart</tr>
-        <tr style={{color:"black"}}>
-          <th>UK</th>
-          <th>36</th>
-          <th>37</th>
-          <th>38</th>
-          <th>39</th>
-          <th>40</th>
-          <th>41</th>
-          <th>42</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr style={{color:"black"}}>
-          <td>IND</td>
-          <td>05</td>
-          <td>06</td>
-          <td>07</td>
-          <td>08</td>
-          <td>09</td>
-          <td>10</td>
-          <td>11</td>
-        </tr>
-      </tbody>
-    </table>
+                  <thead>
+                    <tr style={{ fontSize: "25px", marginTop: "50px", color: "black" }}> Size Chart</tr>
+                    <tr style={{ color: "black" }}>
+                      <th>UK</th>
+                      <th>36</th>
+                      <th>37</th>
+                      <th>38</th>
+                      <th>39</th>
+                      <th>40</th>
+                      <th>41</th>
+                      <th>42</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr style={{ color: "black" }}>
+                      <td>IND</td>
+                      <td>05</td>
+                      <td>06</td>
+                      <td>07</td>
+                      <td>08</td>
+                      <td>09</td>
+                      <td>10</td>
+                      <td>11</td>
+                    </tr>
+                  </tbody>
+                </table>
                 <ProductInfo product={product} />
               </div>
             </div>
@@ -300,7 +306,9 @@ const Product = () => {
       </main>
       <Footer />
     </>
-  ) :<h1>Something went wrong try again</h1>;
+  ) : (
+    <h1>Something went wrong try again</h1>
+  );
 };
 
 export default Product;
